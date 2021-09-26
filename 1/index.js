00001d5381384def46a9f1d0df82909a5c17761a
gilbertomoj <gibamedeirosgc@gmail.com>
2021-09-25 23:22:33 -0300

    Atividades

---
+++ 1/index.js
@@ -0,0 +1,15 @@
+//1
+var nome = 'Gilberto'
+var idade = 20
+var idadeString = idade.toString()
+var info = nome + idadeString
+console.log(info)
+console.log(info.length)
+
+
+
+//2 
+var num1 = 17
+var num2 = 28
+var nums = num1.toString + num2.toString
+console.log(nums)
